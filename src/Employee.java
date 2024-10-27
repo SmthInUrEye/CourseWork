655c88faf622090ebd8ed920b6cb2bad99774b16
SmthInUrEye <dragon-pg@yandex.ru>
2024-10-27 10:53:30 +0300

    CourseWork(Easy level)

---
+++ src/Employee.java
@@ -0,0 +1,57 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Employee {
+    private static final AtomicInteger COUNTER = new AtomicInteger ( 1 );
+    private final int id;
+    private final String name;
+    private double salary;
+    private int department;
+
+    public Employee(String name, double salary, int department) {
+        this.salary = salary;
+        this.department = department;
+        this.name = name;
+        id = COUNTER.getAndIncrement ();
+        ;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public boolean equals(Object other) {
+        if ( this.getClass () != other.getClass () ) {
+            return false;
+        }
+        Employee employee2 = (Employee) other;
+        return name.equals ( employee2.name ) && salary == employee2.salary && department == (employee2.department) && id == employee2.id;
+    }
+
+    public int hashCode() {
+        return java.util.Objects.hash ( name ) + java.util.Objects.hash ( salary ) + java.util.Objects.hash ( department ) + java.util.Objects.hash ( id );
+    }
+
+    public String toString() {
+        return id + "  " + name + "  " + salary + "  " + department + "  ";
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,79 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf ( "Hello and welcome!" );
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println ( "i = " + i );
+    public static void printInfo(Employee[] employees) {
+        System.out.printf ( "%-5s%-25s%-15s%-5s%n", "№", "Name", "Salary", "Department" );
+        for (int i = 0; i < employees.length; i++) {
+            System.out.printf ( "%-5s", employees[i].getId () );
+            System.out.printf ( "%-25s", employees[i].getName () );
+            System.out.printf ( "%-15s", employees[i].getSalary () );
+            System.out.printf ( "%-5s", employees[i].getDepartment () );
+            System.out.println ();
         }
     }
+
+    public static double getMonthlyCosts(Employee[] employees) {
+        double monthlyCosts = 0.0;
+        for (int i = 0; i < employees.length; i++) {
+            monthlyCosts += employees[i].getSalary ();
+        }
+        return monthlyCosts;
+    }
+
+    public static int findMinWageEmployeeId(Employee[] employees) {
+        int MinWageId = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if ( employees[i].getSalary () < employees[MinWageId].getSalary () ) {
+                MinWageId = employees[i].getId () - 1;
+            }
+        }
+        return MinWageId;
+    }
+
+    public static int findMaxWageEmployeeId(Employee[] employees) {
+        int MaxWageId = 0;
+        for (Employee employee : employees) {
+            if ( employee.getSalary () > employees[MaxWageId].getSalary () ) {
+                MaxWageId = employee.getId () - 1;
+            }
+        }
+        return MaxWageId;
+    }
+
+    public static double getAverageSalary(Employee[] employees) {
+        return (getMonthlyCosts ( employees ) / employees.length);
+    }
+
+    public static void printNames(Employee[] employees) {
+        System.out.println ( "Список сотрудников: " );
+        for (int i = 0; i < employees.length; i++) {
+            System.out.println ( " - " + employees[i].getName () );
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Employee[] employee = new Employee[10];
+        employee[0] = new Employee ( "Иван Петрович", 25000.00, 1 );
+        employee[1] = new Employee ( "Петр Иванович", 50000.00, 1 );
+        employee[2] = new Employee ( "Оксана Викторовна", 100000.00, 2 );
+        employee[3] = new Employee ( "Людмила Владимировна", 120000.00, 3 );
+        employee[4] = new Employee ( "Антон Глебович", 150000.00, 4 );
+        employee[5] = new Employee ( "Ринат Башаев", 100000.00, 5 );
+        employee[6] = new Employee ( "Софья Анатольевна", 80000.00, 3 );
+        employee[7] = new Employee ( "Виктор Степанович", 100000.00, 2 );
+        employee[8] = new Employee ( "Талайбек Джайдахалиевич", 15000.00, 4 );
+        employee[9] = new Employee ( "Артур Богданович", 90000.00, 5 );
+
+        printInfo ( employee );
+        System.out.println ( "\nМесячные затраты на зарплату: " + getMonthlyCosts ( employee ) );
+        System.out.println ( "Минимальная зарпалата у сотрудника " + employee[findMinWageEmployeeId ( employee )].getName () + " в размере " + employee[findMinWageEmployeeId ( employee )].getSalary () );
+        System.out.println ( "Максимальная зарпалата у сотрудника " + employee[findMaxWageEmployeeId ( employee )].getName () + " в размере " + employee[findMaxWageEmployeeId ( employee )].getSalary () );
+        System.out.println ( "Средняя зарплата сотрудников: " + getAverageSalary ( employee ) );
+        printNames ( employee );
+
+    }
 }
+
+
+
+
